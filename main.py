ce5f4d17fdd7c143a2a83ba1ed14e4ea74f4f24f
uhhmed <uhhmed@Ahmeds-MacBook-Pro-2.local>
2021-02-06 05:22:18 -0800

    reversed routs.py

--- main.py
+++ main.py
@@ -14,6 +14,17 @@ response = get_data('https://data.cityofnewyork.us/resource/g9ub-hrve.json')
 
 cityData = CityData(response)
 
+@app.route('/phaseCounts')
+def phases_of_projects():
+    phases = cityData.phase_of_projects_counts()
+    return jsonify(phases)
+
+@app.route('/dates')
+def dateCompletion():
+    ordered_data = cityData.dateCompletion()
+    return jsonify(ordered_data)
+
+
 
 
 @app.route('/')
--- routes.py
+++ routes.py
@@ -3,14 +3,3 @@ from main import app
 
 
 
-@app.route('/phaseCounts')
-def phases_of_projects():
-    phases = cityData.phase_of_projects_counts()
-    return jsonify(phases)
-
-@app.route('/dates')
-def dateCompletion():
-    ordered_data = cityData.dateCompletion()
-    return jsonify(ordered_data)
-
-
